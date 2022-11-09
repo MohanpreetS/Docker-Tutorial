2357274e16f3f4ca52cbe5490963dffee1c9ecb6
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-11-09 13:09:04 -0500

    get and post routes

--- mern-docker/backend/index.js
+++ mern-docker/backend/index.js
@@ -25,6 +25,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/api/anime", async (req, res) => {
+  const anime = await Anim.find();
+  res.json(anime);
+});
+
+app.post("/api/anime", async (req, res) => {
+  const anime = new Anim(req.body);
+  await anime.save();
+  res.json(anime);
+});
 
 app.listen(8000, () => {
   console.log("server listening on port 8000");
