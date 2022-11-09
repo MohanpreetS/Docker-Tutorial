bdc99dd94c015b6114c06b3ad4c265570f8d8fad
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-11-09 13:09:04 -0500

    running express and the home route

--- mern-docker/backend/index.js
+++ mern-docker/backend/index.js
@@ -9,6 +9,22 @@ colors.enable();
 const Anim = require("./database/anim.model");
 const connect = require("./database/connect");
 
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log("Hello World!".rainbow);
+
+  res.send("Hello World!");
+});
+
 
 app.listen(8000, () => {
   console.log("server listening on port 8000");
