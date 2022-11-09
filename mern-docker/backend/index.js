937fa27e3ccc09ee1fbdc1090c2035413aa1e650
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-11-09 13:09:04 -0500

    adding some mode dependencies

--- mern-docker/backend/index.js
+++ mern-docker/backend/index.js
@@ -1,4 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const colors = require("colors");
+
+dotenv.config();
+colors.enable();
+
+const Anim = require("./database/anim.model");
+const connect = require("./database/connect");
+
 
 app.listen(8000, () => {
   console.log("server listening on port 8000");
