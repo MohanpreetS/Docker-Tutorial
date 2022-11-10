0d6b1463abded4830024b385bfaa4e21458d9a25
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-11-10 13:09:04 -0500

    docker file for the backend

--- mern-docker/backend/database/connect.js
+++ mern-docker/backend/database/connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 function connect() {
   mongoose
-    .connect(process.env.DB_URL || 'mongodb://localhost:27017/anime')
+    .connect(process.env.DB_URL||'mongodb://localhost:27017/anime')
     .then(() => {
       console.log("Successfully connected to database");
     })
