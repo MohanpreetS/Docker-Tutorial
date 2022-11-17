cd286b53b7c31d045787a2ca1d3dd079ca2879ca
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-11-17 13:09:04 -0500

    create task in  db

--- next-docker/lib/action.ts
+++ next-docker/lib/action.ts
@@ -13,3 +13,20 @@ export async function getTasks() {
     console.log(err);
   }
 }
+
+export async function createTask(params: {
+  task: string;
+  description: string;
+  status: string;
+}) {
+  await dbConnect();
+
+  const { task, description, status } = params;
+
+  try {
+    const newTask = await Task.create({ task, description, status });
+    return newTask;
+  } catch (err) {
+    console.log(err);
+  }
+}
